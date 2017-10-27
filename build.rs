fa53cbc1871504b85193c0c2fb1e1b63075ab1a0
Simon Sapin <simon.sapin@exyr.org>
2017-10-27 23:00:20 +0200

    Build pixman

--- build.rs
+++ build.rs
@@ -1,33 +1,60 @@
 use std::env;
+use std::fs::create_dir;
 use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
+    let pixman_subdir = "pixman-0.34.0";
     let cairo_subdir = "cairo-1.15.8";
-    let disabled_features = r#"
+    let disabled_cairo_features = r#"
         xlib xcb qt quartz win32 skia os2 beos drm gallium gl directfb gobject fc ft
     "#;
 
     let src_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let cairo_dir = src_dir.join(cairo_subdir);
-    let libs_dir = out_dir.join("src").join(".libs");
+    let pixman_src_dir = src_dir.join(pixman_subdir);
+    let pixman_out_dir = out_dir.join("pixman");
+    let pixman_libs_dir = pixman_out_dir.join("pixman").join(".libs");
+    let cairo_src_dir = src_dir.join(cairo_subdir);
+    let cairo_out_dir = out_dir.join("cairo");
+    let cairo_libs_dir = cairo_out_dir.join("src").join(".libs");
 
-    run(Command::new(cairo_dir.join("configure"))
-        .args(disabled_features.split_whitespace().map(|f| format!("--disable-{}", f)))
-        .current_dir(&out_dir)
+    create_dir_if_not_exists(&pixman_out_dir);
+    create_dir_if_not_exists(&cairo_out_dir);
+
+    run(Command::new(pixman_src_dir.join("configure"))
+        .arg("--disable-gtk")
+        .current_dir(&pixman_out_dir)
     );
     run(Command::new("make")
         .env("MAKEFLAGS", env::var_os("CARGO_MAKEFLAGS").unwrap_or_default())
-        .current_dir(&out_dir)
+        .current_dir(&pixman_out_dir)
     );
 
-    println!("cargo:rerun-if-changed={}", cairo_dir.to_str().unwrap());
-    println!("cargo:rustc-link-search=native={}", libs_dir.to_str().unwrap());
+    run(Command::new(cairo_src_dir.join("configure"))
+        .args(disabled_cairo_features.split_whitespace().map(|f| format!("--disable-{}", f)))
+        .current_dir(&cairo_out_dir)
+    );
+    run(Command::new("make")
+        .env("MAKEFLAGS", env::var_os("CARGO_MAKEFLAGS").unwrap_or_default())
+        .current_dir(&cairo_out_dir)
+    );
+
+    println!("cargo:rerun-if-changed={}", cairo_src_dir.to_str().unwrap());
+    println!("cargo:rerun-if-changed={}", pixman_src_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search=native={}", cairo_libs_dir.to_str().unwrap());
+    println!("cargo:rustc-link-search=native={}", pixman_libs_dir.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=cairo");
+    println!("cargo:rustc-link-lib=static=pixman-1");
 }
 
 fn run(command: &mut Command) {
     println!("Running {:#?}", command);
     assert!(command.status().unwrap().success());
+}
+
+fn create_dir_if_not_exists(path: &PathBuf) {
+    if !path.exists() {
+        create_dir(&path).unwrap()
+    }
 }
