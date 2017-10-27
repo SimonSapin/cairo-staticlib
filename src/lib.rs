602ada096e131023357630bbcd996ba6ccf9337a
Simon Sapin <simon.sapin@exyr.org>
2017-10-27 20:35:55 +0200

    Add FFI bindings generated by rust-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 #![no_std]
+#![allow(non_camel_case_types)]
 
 extern crate libc;
-use libc::c_char;
 
-#[link(name = "cairo", kind = "static")]
-extern "C" {
-    pub fn cairo_version_string() -> *const c_char;
-}
+include!("bindings.rs");
